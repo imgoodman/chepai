80aa21ffea3c3cb4f226904095f4860ff221071f
imgoodman <jan8611@163.com>
2016-11-28 20:41:49 +0800

    all bid data txt

--- code/confidentials.py
+++ code/confidentials.py
@@ -1,2 +1,2 @@
 def getMySqlAuth():
-    return ("qdm115145384.my3w.com","qdm115145384","***","qdm115145384_db")
+    return ("qdm115145384.my3w.com","qdm115145384","****","qdm115145384_db")
--- code/data.py
+++ code/data.py
@@ -212,8 +212,28 @@ def getBidDataJsonFromDB(yearStart=2016,yearEnd=2016):
         file_bid_data.write(json.dumps(data))
         file_bid_data.close()
 
+def getAllBidData():
+    secrets=confidentials.getMySqlAuth()
+    conn=pymysql.connect(host=secrets[0],user=secrets[1],passwd=secrets[2],db=secrets[3])
+    cur=conn.cursor()
+    cur.execute('use ' +secrets[3])
+    sql='select a.bid_month,a.system_time,a.lowest_price,b.alert_price,b.bid_people_num,b.license_num,b.lowest_price as final_lowest_price from t_bid_data as a left join t_bid_summary as b on a.bid_month=b.bid_month where b.alert_price>0 and a.system_time>="11:29:30" and a.system_time<="11:29:59" order by a.bid_month desc,a.system_time asc'
+    cur.execute(sql)
+    data=''
+    rows=cur.fetchall()
+    for r in rows:
+        print(r)
+        t=r[1].split(':')[2]
+        data+=r[0]+'\t'+t+'\t'+str(r[2])+'\t'+str(r[3])+'\t'+str(r[4])+'\t'+str(r[5])+'\t'+str(r[6])+'\n'
+    data_file=open('../data/bid_data_all.txt','w')
+    data_file.write(data)
+    data_file.close()
+    cur.close()
+    conn.close()
+
 #beginScrapyData()
 #scrapySummaryData()
 #getJsonFromSummaryData()
 #getBidDataFromDB(yearStart=2013,yearEnd=2016)
-getBidDataJsonFromDB(yearStart=2013,yearEnd=2016)
+#getBidDataJsonFromDB(yearStart=2013,yearEnd=2016)
+getAllBidData()
