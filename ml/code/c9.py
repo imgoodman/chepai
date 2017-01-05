f421a6b8d4bd597d2ddf3a6ce1f8c9b14451d601
imgoodman <jan8611@163.com>
2017-01-05 17:00:14 +0800

    prepare to get data at home

--- ml/code/c9.py
+++ ml/code/c9.py
@@ -5,6 +5,11 @@ from urllib.error import HTTPError
 
 import numpy as np
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import SVC
+from sklearn.cross_validation import cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn import grid_search
 
 titles={}
 
@@ -45,6 +50,54 @@ url_base='http://www.gutenberg.org/files'
 url_format='{url_base}/{id}/{id}-0.txt'
 
 data_folder='../data/book/'
+
+function_words = ["a", "able", "aboard", "about", "above", "absent",
+                  "according" , "accordingly", "across", "after", "against",
+                  "ahead", "albeit", "all", "along", "alongside", "although",
+                  "am", "amid", "amidst", "among", "amongst", "amount", "an",
+                    "and", "another", "anti", "any", "anybody", "anyone",
+                    "anything", "are", "around", "as", "aside", "astraddle",
+                    "astride", "at", "away", "bar", "barring", "be", "because",
+                    "been", "before", "behind", "being", "below", "beneath",
+                    "beside", "besides", "better", "between", "beyond", "bit",
+                    "both", "but", "by", "can", "certain", "circa", "close",
+                    "concerning", "consequently", "considering", "could",
+                    "couple", "dare", "deal", "despite", "down", "due", "during",
+                    "each", "eight", "eighth", "either", "enough", "every",
+                    "everybody", "everyone", "everything", "except", "excepting",
+                    "excluding", "failing", "few", "fewer", "fifth", "first",
+                    "five", "following", "for", "four", "fourth", "from", "front",
+                    "given", "good", "great", "had", "half", "have", "he",
+                    "heaps", "hence", "her", "hers", "herself", "him", "himself",
+                    "his", "however", "i", "if", "in", "including", "inside",
+                    "instead", "into", "is", "it", "its", "itself", "keeping",
+                    "lack", "less", "like", "little", "loads", "lots", "majority",
+                    "many", "masses", "may", "me", "might", "mine", "minority",
+                    "minus", "more", "most", "much", "must", "my", "myself",
+                    "near", "need", "neither", "nevertheless", "next", "nine",
+                    "ninth", "no", "nobody", "none", "nor", "nothing",
+                    "notwithstanding", "number", "numbers", "of", "off", "on",
+                    "once", "one", "onto", "opposite", "or", "other", "ought",
+                    "our", "ours", "ourselves", "out", "outside", "over", "part",
+                    "past", "pending", "per", "pertaining", "place", "plenty",
+                    "plethora", "plus", "quantities", "quantity", "quarter",
+                    "regarding", "remainder", "respecting", "rest", "round",
+                    "save", "saving", "second", "seven", "seventh", "several",
+                    "shall", "she", "should", "similar", "since", "six", "sixth",
+                    "so", "some", "somebody", "someone", "something", "spite",
+                    "such", "ten", "tenth", "than", "thanks", "that", "the",
+                    "their", "theirs", "them", "themselves", "then", "thence",
+                  "therefore", "these", "they", "third", "this", "those",
+"though", "three", "through", "throughout", "thru", "thus",
+"till", "time", "to", "tons", "top", "toward", "towards",
+"two", "under", "underneath", "unless", "unlike", "until",
+"unto", "up", "upon", "us", "used", "various", "versus",
+"via", "view", "wanting", "was", "we", "were", "what",
+"whatever", "when", "whenever", "where", "whereas",
+"wherever", "whether", "which", "whichever", "while",
+                  "whilst", "who", "whoever", "whole", "whom", "whomever",
+"whose", "will", "with", "within", "without", "would", "yet",
+"you", "your", "yours", "yourself", "yourselves"]
 
 def get_data():
     for author in titles:
@@ -92,6 +145,18 @@ def load_book_data():
                 authors.append(author_number)
     return documents,np.array(authors,dtype='int')
 
+def classify_document():
+    documents,labels = load_book_data()
+
+    extractor = CountVectorizer(vocabulary=function_words)
+    parameters={'kernel':('linear','rbf'),'C':[1,10]}
+    svc=SVC()
+    grid=grid_search.GridSearchCV(svc,parameters)
+
+    pipeline1= Pipeline([('feature_extraction',extractor),('clf',grid)])
+
+    scores= cross_val_score(pipeline1,documents,labels,scoring='f1')
+    print(np.mean(scores))
 
 if __name__=='__main__':
-    load_book_data()
+    get_data()
