8c95ab1c917693c2bef7eb6223cfe1da51d3bbe9
imgoodman <jan8611@163.com>
2017-01-22 14:54:44 +0800

    no

--- code/confidentials.py
+++ code/confidentials.py
@@ -1,6 +1,6 @@
 def getMySqlAuth():
-    return ("qdm115145384.my3w.com","qdm115145384","********","qdm115145384_db")
+    return ("********.my3w.com","********","********","*******")
 
 
 def getLocalSqlAuth():
-    return ("192.168.1.5","root","**********","51zhishi")
+    return ("192.168.1.5","root","**********","********")
