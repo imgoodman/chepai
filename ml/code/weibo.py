7be1dcf4f0385815b4efc94c3c7695a211f131fc
imgoodman <jan8611@163.com>
2017-01-23 13:53:45 +0800

    weibo knn

--- ml/code/weibo.py
+++ ml/code/weibo.py
@@ -18,21 +18,30 @@ def get_weibo(userId):
     labels=pipeline.predict(weibos)
     #labels由数字0-9组成的，由n_clusters导致的。
     #labels中的每个数字，表示对应的数据点是属于那个族
+    terms = pipeline.named_steps["feature_extraction"].get_feature_names()
+
     print(labels)
     c=Counter(labels)
     print(c)
     for i in range(n_clusters):
         print("cluster {0} has {1} samples".format(i,c[i]))
-    print(pipeline.named_steps['cluster'].inertia_)
+        print('most important terms:')
+        centroid=pipeline.named_steps['cluster'].cluster_centers_[i]#数组 每个族的质心
+        most_important = centroid.argsort()
+        for i in range(5):
+            term_index = most_important[-(i+1)]
+            print('{0}  {1} (score: {2:.4f})'.format(i+1,terms[term_index],centroid[term_index]))
+    print(pipeline.named_steps['cluster'].inertia_)    
 
 def multi_cluster_weibo(userId):
     documents=db.load_weibo_data(userId)
     inertia_scores=[]
     for n in range(2,20):
         cur_inertia_score=[]
-        X = TfidfVectorizer(max_df=0.4).fit_transform(documents)
+        X = TfidfVectorizer(max_df=0.4).fit_transform(documents)        
         for i in range(10):
             km=KMeans(n_clusters=n).fit(X)
+            print('inertia of {0}th in {1} cluster is :{2}'.format(i,n,km.inertia_))
             cur_inertia_score.append(km.inertia_)
         inertia_scores.append(np.mean(cur_inertia_score))
     print(inertia_scores)
@@ -40,4 +49,4 @@ def multi_cluster_weibo(userId):
     plt.show()
 
 if __name__=="__main__":
-    multi_cluster_weibo("gaoxiaosong")
+    get_weibo("gaoxiaosong")
